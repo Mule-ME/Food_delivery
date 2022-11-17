402eb3d3e1ed9ce02213ad6e4be91cba4b5c5548
Mulualem Eshetu <mulualem@ienetworks.co>
2022-11-17 13:56:59 +0300

    Card recipe  compnenet implementation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./public/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#ff6363",
+        secondary: {
+          100: "#e2e2d5",
+          200: "#888883",
+        },
+      },
+    },
+
+    fontFamily: {
+      body: ["Lato"],
+    },
   },
   plugins: [],
 };
